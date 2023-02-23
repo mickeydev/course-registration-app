e02e61ab5d9711ad673e156acca3090b590fbd9d
Michael Yeboah Frimpong <46923296+mickeydev@users.noreply.github.com>
2023-02-23 10:06:12 +0530

    changes

--- frontend/src/section/AddSections/InstructorForm.tsx
+++ frontend/src/section/AddSections/InstructorForm.tsx
@@ -1,56 +1,86 @@
-import { Box, Typography, TextField, Button } from "@mui/material"
-import { ChangeEvent, FormEvent, useState } from "react"
-import axios from "../../axiosConfig";
-import AddInstructor from "../../types/Instructor";
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import axios from '../../axiosConfig';
+import AddInstructor from '../../types/Instructor';
 import { useSnackbar } from 'notistack';
 
 function InstructorForm() {
-    const [instructorFormDetails, setInstructorFormDetails] = useState<AddInstructor>({
-        title: "",
-        name: "",
-        department: ""
-    });
+   const [instructorFormDetails, setInstructorFormDetails] =
+      useState<AddInstructor>({
+         title: '',
+         name: '',
+         department: '',
+      });
 
-    const { enqueueSnackbar } = useSnackbar();
+   const { enqueueSnackbar } = useSnackbar();
 
-    function OnChangeHandler(e: ChangeEvent<HTMLInputElement>) {
-        setInstructorFormDetails((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }))
-    }
+   function OnChangeHandler(e: ChangeEvent<HTMLInputElement>) {
+      setInstructorFormDetails((prev) => ({
+         ...prev,
+         [e.target.name]: e.target.value,
+      }));
+   }
 
-    async function OnSubmitHandler(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        try {
-            const response = await axios.post("/add-instructor", instructorFormDetails)
-            enqueueSnackbar(response.data, { variant: 'success' })
-            console.log(response);
-        } catch (e: any) {
-            console.log(e)
-        }
-    }
+   async function OnSubmitHandler(e: FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+      try {
+         const response = await axios.post(
+            '/add-instructor',
+            instructorFormDetails
+         );
+         enqueueSnackbar(response.data, { variant: 'success' });
+         console.log(response);
+      } catch (e: any) {
+         console.log(e);
+      }
+   }
 
-    return (
-        <Box m={2}>
-            <Typography sx={{ mb: 3 }}>Add Instructor</Typography>
-            <form onSubmit={OnSubmitHandler}>
-                <TextField name="title" label="Title" type="text" variant="standard" fullWidth InputLabelProps={{
-                    shrink: true,
-                }} onChange={OnChangeHandler} />
-                <TextField name="name" label="Full Name" type="text" variant="standard" sx={{ mt: 2 }} fullWidth InputLabelProps={{
-                    shrink: true,
-                }} onChange={OnChangeHandler} />
-                <TextField name="department" label="Department" type="text" variant="standard" sx={{ mt: 2 }} fullWidth InputLabelProps={{
-                    shrink: true,
-                }} onChange={OnChangeHandler} />
+   return (
+      <Box m={2}>
+         <Typography sx={{ mb: 3 }}>Add Instructor</Typography>
+         <form onSubmit={OnSubmitHandler}>
+            <TextField
+               name="title"
+               label="Title"
+               type="text"
+               variant="standard"
+               fullWidth
+               InputLabelProps={{
+                  shrink: true,
+               }}
+               onChange={OnChangeHandler}
+            />
+            <TextField
+               name="name"
+               label="Full Name"
+               type="text"
+               variant="standard"
+               sx={{ mt: 2 }}
+               fullWidth
+               InputLabelProps={{
+                  shrink: true,
+               }}
+               onChange={OnChangeHandler}
+            />
+            <TextField
+               name="department"
+               label="Department"
+               type="text"
+               variant="standard"
+               sx={{ mt: 2 }}
+               fullWidth
+               InputLabelProps={{
+                  shrink: true,
+               }}
+               onChange={OnChangeHandler}
+            />
 
-                <Button type="submit" variant="contained" sx={{ mt: 3 }}>Add</Button>
-            </form>
-
-        </Box>
-    )
+            <Button type="submit" variant="contained" sx={{ mt: 3 }}>
+               Add
+            </Button>
+         </form>
+      </Box>
+   );
 }
 
-
-export default InstructorForm
+export default InstructorForm;
--- frontend/src/store/register-context.tsx
+++ frontend/src/store/register-context.tsx
@@ -1,88 +1,88 @@
-import React, { createContext, useState, useEffect, ReactElement, JSXElementConstructor } from "react";
-import contextType from "../types/contextType";
-import { courseDetails } from "../types/Course";
-import axios from "../axiosConfig";
-import { ViewStudent } from "../types/Student";
-import { ViewInstructor } from "../types/Instructor";
+import React, {
+   createContext,
+   useState,
+   useEffect,
+   ReactElement,
+   JSXElementConstructor,
+} from 'react';
+import contextType from '../types/contextType';
+import { courseDetails } from '../types/Course';
+import axios from '../axiosConfig';
+import { ViewStudent } from '../types/Student';
+import { ViewInstructor } from '../types/Instructor';
 
 const initialValues: contextType = {
-    courseCount: 0,
-    studentCount: 0,
-    instructorCount: 0,
-    courseData: [],
-    studentData: [],
-    instructorData: [],
-}
+   courseCount: 0,
+   studentCount: 0,
+   instructorCount: 0,
+   courseData: [],
+   studentData: [],
+   instructorData: [],
+};
 
 export const RegContext = createContext<contextType>(initialValues);
 
-
 type Props = {
-    children: string | JSX.Element | JSX.Element[]
-}
+   children: string | JSX.Element | JSX.Element[];
+};
 
 function RegContextProvider({ children }: Props) {
-    const [courseData, setCourseData] = useState<courseDetails[]>([]);
-    const [studentData, setStudentData] = useState<ViewStudent[]>([]);
-    const [instructorData, setInstructorData] = useState<ViewInstructor[]>([]);
+   const [courseData, setCourseData] = useState<courseDetails[]>([]);
+   const [studentData, setStudentData] = useState<ViewStudent[]>([]);
+   const [instructorData, setInstructorData] = useState<ViewInstructor[]>([]);
 
-    async function CourseViewGet() {
-        try {
-            const response = await axios.get("/view-courses");
+   async function CourseViewGet() {
+      try {
+         const response = await axios.get('/view-courses');
 
-            setCourseData(response.data)
-        } catch (error: any) {
-            console.log(error);
-        }
+         setCourseData(response.data);
+      } catch (error: any) {
+         console.log(error);
+      }
+   }
 
-    }
+   useEffect(() => {
+      CourseViewGet();
+   }, []);
 
-    useEffect(() => { CourseViewGet(); }, []);
+   async function StudentViewGet() {
+      try {
+         const response = await axios.get('/view-students');
 
+         setStudentData(response.data);
+      } catch (error: any) {
+         console.log(error);
+      }
+   }
 
-    async function StudentViewGet() {
-        try {
-            const response = await axios.get("/view-students");
+   useEffect(() => {
+      StudentViewGet();
+   }, []);
 
-            setStudentData(response.data)
-        } catch (error: any) {
-            console.log(error);
-        }
+   async function InstructorViewGet() {
+      try {
+         const response = await axios.get('/view-instructors');
 
-    }
+         setInstructorData(response.data);
+      } catch (error: any) {
+         console.log(error);
+      }
+   }
 
-    useEffect(() => { StudentViewGet(); }, []);
+   useEffect(() => {
+      InstructorViewGet();
+   }, []);
 
+   const values = {
+      courseCount: courseData.length,
+      studentCount: studentData.length,
+      instructorCount: instructorData.length,
+      courseData: courseData,
+      studentData: studentData,
+      instructorData: instructorData,
+   };
 
-    async function InstructorViewGet() {
-        try {
-            const response = await axios.get("/view-instructors");
-
-            setInstructorData(response.data)
-        } catch (error: any) {
-            console.log(error);
-        }
-
-    }
-
-    useEffect(() => { InstructorViewGet(); }, []);
-
-
-    const values = {
-        courseCount: courseData.length,
-        studentCount: studentData.length,
-        instructorCount: instructorData.length,
-        courseData: courseData,
-        studentData: studentData,
-        instructorData: instructorData,
-    }
-
-    return (
-        <RegContext.Provider value={values}>
-            {children}
-        </RegContext.Provider>
-    )
+   return <RegContext.Provider value={values}>{children}</RegContext.Provider>;
 }
 
-
-export default RegContextProvider
+export default RegContextProvider;
